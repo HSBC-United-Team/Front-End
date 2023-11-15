fcc3ba953f2fc11d7d3267dd8eda8961f6903882
geofanno <geofanno710@gmail.com>
2023-11-15 15:14:47 +0800

    page/cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 // import HomePage from "../src/Components/Pages/Homepage";
 // import Fav from "./Components/Pages/Favoritepage";
-import LandingPage from "./Components/Pages/LandingPage";
+import Cart from "./Components/Pages/CartPage";
+// import LandingPage from "./Components/Pages/LandingPage";
 // import LoginPage from "./Components/Pages/LoginPage";
 // import SignUpPage from "./Components/Pages/SignUpPage";
 
 function App() {
     return (
         <>
-            <LandingPage />
-            {/* <LoginPage /> */}
-            {/* <SignUpPage /> */}
-            {/* <HomePage /> */}
+            {/* <LandingPage />
+            <LoginPage />
+            <SignUpPage />
+            <HomePage /> */}
             {/* <Fav /> */}
+            <Cart/>
         </>
     );
 }
