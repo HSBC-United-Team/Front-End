dd6ebf508114c283030baced8f511bc26b35bb9d
Ridho-Chaniago <ridho12c.rc@gmail.com>
2023-11-15 23:30:51 +0700

    menyesuaikan addbutton dengan api

--- src/Components/molecules/ProductCard.jsx
+++ src/Components/molecules/ProductCard.jsx
@@ -1,60 +1,71 @@
-import PropTypes from "prop-types";
 import BtnAddProduct from "../atoms/BtnAddProdcut";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { UseCartData } from "../../stores/useCartData";
 
-function ProductCard(props) {
-    const { product } = props;
-    const {addProductToCart,increaseProductAmount}=UseCartData((UseCart)=>{
-    return{
+function ProductCard() {
+    const [productList, setProductList] = useState([]);
+    const { addProductToCart } = UseCartData((UseCart) => {
+        return {
 
-        addProductToCart:UseCart.addProductToCart,
-        increaseProductAmount:useCart.increaseProductAmount
-    }}
+            addProductToCart: UseCart.addProductToCart
+        }
+    }
     )
-    // const { addProductToCart } = UseCartData();
-    const handleAddToCart = () => {
+
+    const handleAddToCarts = (product) => {
+
         addProductToCart(
-          product
+            product
         );
-      };
 
 
+    };
+
+    useEffect(() => {
+        axios.get("https://dummyjson.com/products").then((res) => {
+            setProductList(res.data.products);
+        });
+    }, []);
     return (
         <>
-            <div className="flex flex-col justify-beetwen p-4 h-64 border-[1px] border-[#E2E2E2] rounded-2xl hover:scale-105 hover:border-green-800">
-                <button className="flex h-[55%] mx-auto my-auto">
-                    <img
-                        className="max-h-full mx-auto my-auto"
-                        src={product.image}
-                        alt="Product Picture"
-                    />
-                </button>
-                <div className=" h-[45%]">
-                    <h3 className="nama font-semibold">{product.name}</h3>
-                    <p className="text-[#7C7C7C] text-[14px]">
-                        {product.weight}
-                    </p>
-                    <div className="flex justify-between items-end">
-                        <span className="harga font-semibold">
-                            {product.price}
-                        </span>
-                        <BtnAddProduct onClick={handleAddToCart} />
-                    </div>
-                </div>
-            </div>
+            {productList.length < 1 ? (
+                <h1>Loading...</h1>
+            ) : (
+                productList.map((product) => {
+                    // console.log(product)
+                    return (
+                        <>
+                            <div className="flex flex-col justify-beetwen p-4 h-64 border-[1px] border-[#E2E2E2] rounded-2xl hover:scale-105 hover:border-green-800">
+                                <button className="flex h-[55%] mx-auto my-auto">
+                                    <img
+                                        className="max-h-full mx-auto my-auto"
+                                        src={product.thumbnail}
+                                        alt="Product Picture"
+                                    />
+                                </button>
+                                <div className=" h-[45%]">
+                                    <h3 className="nama font-semibold">{product.title}</h3>
+                                    <p className="text-[#7C7C7C] text-[14px]">
+                                        {product.category}
+                                    </p>
+                                    <div className="flex justify-between items-end">
+                                        <span className="harga font-semibold">
+                                            ${product.price}
+                                        </span>
+                                        <BtnAddProduct onClick={() =>
+                                            // remove
+                                            handleAddToCarts(product)
+                                        } />
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    );
+                })
+            )}
         </>
     );
 }
-
-ProductCard.propTypes = {
-    product: PropTypes.shape({
-        image: PropTypes.string.isRequired,
-        weight: PropTypes.string.isRequired,
-        price: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-    }),
-};
 
 export default ProductCard;
--- src/stores/useCartData.js
+++ src/stores/useCartData.js
@@ -2,60 +2,65 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 const UseCart = (set) => ({
-    cartData: [],
-    addProductToCart: (product) => {
-      console.log("nyambung")
-        set((state) => {
-            const updatedCartData = [...state.cartData];
-            console.log(updatedCartData)
-            const productIndex = updatedCartData.findIndex(
-                (prod) => prod.productName === product.name
-            );
-            if (productIndex !== -1) {
-                // state.increaseProductAmount(productIndex);
-                updatedCartData[productIndex].productAmount +=1
-                console.log(`quantity ${updatedCartData[productIndex].productName} ditambah 1 menjadi ${updatedCartData[productIndex].productAmount}`)
-                return{cartData:updatedCartData}
-            } else {
-                updatedCartData.push({
-                    productName:product.name,
-                    productDescription:product.weight,
-                    productAmount: 1,
-                    productPrice:product.price,
-                    productImage:product.image,
-                });
-                console.log("item baru di tambahkan ke keranjang")
-                return { cartData: updatedCartData };
-            }
+  cartData: [],
+  addProductToCart: (product) => {
+    console.log("nyambung");
+    console.log(product)
+    set((state) => {
+      const updatedCartData = [...state.cartData];
+      console.log(updatedCartData);
+      const productIndex = updatedCartData.findIndex(
+        (prod) => prod.productName === product.title
+      );
+      if (productIndex !== -1) {
+        // state.increaseProductAmount(productIndex);
+        console.log(productIndex)
+        updatedCartData[productIndex].productAmount += 1;
+        console.log(
+          `quantity ${updatedCartData[productIndex].productName} ditambah 1 menjadi ${updatedCartData[productIndex].productAmount}`
+        );
+        return { cartData: updatedCartData };
+      } else {
+        updatedCartData.push({
+          id:product.id,
+          productName: product.title,
+          productDescription: product.category,
+          productAmount: 1,
+          productPrice: product.price,
+          productImage: product.thumbnail,
         });
-    },
-    removeProductInCart: (productIndex) => {
-        set((state) => {
-            const updatedCartData = [...state.cartData];
-            updatedCartData.splice(productIndex, 1);
-            return { cartData: updatedCartData };
-        });
-    },
-    increaseProductAmount: (productIndex) => {
-        set((state) => {
-            const updatedCartData = [...state.cartData];
-            updatedCartData[productIndex] = {
-                ...updatedCartData[productIndex],
-                productAmount: state.cartData[productIndex].productAmount + 1,
-            };
-            return { cartData: updatedCartData };
-        });
-    },
-    decreaseProductAmount: (productIndex) => {
-        set((state) => {
-            const updatedCartData = [...state.cartData];
-            updatedCartData[productIndex] = {
-                ...updatedCartData[productIndex],
-                productAmount: state.cartData[productIndex].productAmount - 1,
-            };
-            return { cartData: updatedCartData };
-        });
-    },
+        console.log("item baru di tambahkan ke keranjang");
+        return { cartData: updatedCartData };
+      }
+    });
+  },
+  removeProductInCart: (productIndex) => {
+    set((state) => {
+      const updatedCartData = [...state.cartData];
+      updatedCartData.splice(productIndex, 1);
+      return { cartData: updatedCartData };
+    });
+  },
+  increaseProductAmount: (productIndex) => {
+    set((state) => {
+      const updatedCartData = [...state.cartData];
+      updatedCartData[productIndex] = {
+        ...updatedCartData[productIndex],
+        productAmount: state.cartData[productIndex].productAmount + 1,
+      };
+      return { cartData: updatedCartData };
+    });
+  },
+  decreaseProductAmount: (productIndex) => {
+    set((state) => {
+      const updatedCartData = [...state.cartData];
+      updatedCartData[productIndex] = {
+        ...updatedCartData[productIndex],
+        productAmount: state.cartData[productIndex].productAmount - 1,
+      };
+      return { cartData: updatedCartData };
+    });
+  },
 });
 
-export const UseCartData = create(persist(UseCart, { name: "use-cart-data2" }));
+export const UseCartData = create(persist(UseCart, { name: "use-cart-data" }));
