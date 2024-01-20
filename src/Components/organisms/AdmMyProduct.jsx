86e8d6168475f437073766e83a06988f1e8b8762
dhenmasadhi <dhenmasa@gmail.com>
2024-01-20 22:09:46 +0700

    menambahkan search

--- src/Components/organisms/AdmMyProduct.jsx
+++ src/Components/organisms/AdmMyProduct.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import MyProduct from "../molecules/MyProduct";
 import AddProductModal from "./addProductModal";
+import SearchMyProduct from "../molecules/SearchMyProduct";
 
 function AdmMyProduct() {
   const [openModalAddProduct, setOpenModalAddProduct] = useState(false);
@@ -9,12 +10,15 @@ function AdmMyProduct() {
       <div className="m-auto w-[90%]">
         <div className="flex justify-between">
           <h1 className="text-2xl font-semibold pt-4">My Products</h1>
-          <button
-            className="border-2 px-5 py-3 rounded-lg bg-green-500 text-white font-medium mb-2"
-            onClick={() => setOpenModalAddProduct(true)}
-          >
-            Add Product
-          </button>
+          <div className="flex items-center gap-5">
+            <SearchMyProduct />
+            <button
+              className="border-2 px-5 py-3 rounded-lg bg-green-500 text-white font-medium mb-2"
+              onClick={() => setOpenModalAddProduct(true)}
+            >
+              Add Product
+            </button>
+          </div>
         </div>
         <div className="border rounded-lg p-3 border-green-500">
           <MyProduct />
