aab830bc0b4c22f7e2fe3a80494130549e587e3e
Ridho-Chaniago <ridho12c.rc@gmail.com>
2024-01-22 00:31:04 +0700

    connect api product, slicing tracking order

--- src/Components/atoms/AccountSection.jsx
+++ src/Components/atoms/AccountSection.jsx
@@ -1,15 +1,17 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
-function AccountSection({ title, iconSrc, onClick }) {
+function AccountSection({ title, iconSrc, onClick,isClicked }) {
+  
   return (
-    <div className="flex md:px-16 px-7 md:py-5 py-7 justify-between border-b-2">
+      <div className={`flex md:px-16 px-7 md:py-5 py-7 justify-between ${isClicked?"":"border-b-2"}`}>
       <button className='flex justify-between w-full'onClick={onClick}>
         <div className="flex gap-4">
           <img className="w-7" src={iconSrc} alt="" />
           <p className="font-bold text-[18px]">{title}</p>
         </div>
           <img src="public\images\svg\backarrow.png" alt="" />
+       
       </button>
 
     </div>
---
+++ src/Components/atoms/BtnTrackOrder.jsx
@@ -0,0 +1,11 @@
+const BtnTrackOrder = ({src, children}) => {
+    return (
+        <button className="flex justify-center items-center flex-col hover:scale-105">
+            <div className="w-12 h-12 rounded-full flex justify-center items-center border border-black bg-white">
+                <img src={src} className="w-8 h-8" alt="" />
+            </div>
+            <p>{children}</p>
+        </button>
+    )
+}
+export default BtnTrackOrder
